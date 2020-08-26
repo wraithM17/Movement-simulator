822e4af50b83d1ef56c5e4d6355a6dce74492fd3
wraithM17 <37100541+wraithM17@users.noreply.github.com>
2020-08-26 20:02:11 +0530

    Add files via upload

--- movement.js
+++ movement.js
@@ -2,9 +2,21 @@ var entity_x = 200;
 var entity_y = 150;
 var ENTITY = document.getElementById("entity");
 var moving = false;
+var short_move;
+var move_x;
+var move_y;
+
+
+
 
 
 function mouse_coords(event){
+	if (moving){
+
+
+	}
+
+
 	if (!moving){
 		console.log("Moving now.");
 	var x = event.clientX;
@@ -23,13 +35,11 @@ function mouse_coords(event){
 function track(x,y){
 	x -= 15;
 	y -= 15;
-	//ENTITY.style.left = x;
-	//ENTITY.style.top = y;
-	//ENTITY.style.backgroundColor = random_color();
-	
-	path(x,y);
+	shortest_path(x,y);
 }
 
+
+// LONG PATH FUNCTION OBSOLETE NOW RIP ----!----
 function path(x,y){
 	moving = true;
 
@@ -71,6 +81,140 @@ function path(x,y){
 
 
 }
+
+// I would like to thank my friends, my family , my braincells ,
+// my maths , my country , my planet for making this function
+// possible
+function shortest_path(x,y){
+	console.log(`X : ${x}   Y : ${y}`);
+	moving = true;
+	x_diff = Math.abs(entity_x-x);
+	y_diff = Math.abs(entity_y-y);
+
+	
+	if (x_diff > y_diff){
+		let short_move = setInterval(function(){
+			$("#display_coords").html(`X : ${entity_x} &nbsp;&nbsp;&nbsp;Y : ${entity_y}`);
+			if (entity_y != y){
+				if (entity_y > y){
+					entity_y -= 1;
+					if (entity_x > x){
+						entity_x -= 1;
+					}
+					else if (entity_x < x){
+						entity_x += 1;
+					}
+				}
+				else if (entity_y < y){
+					entity_y += 1;
+					if (entity_x > x){
+						entity_x -= 1;
+					}
+					else if (entity_x < x){
+						entity_x += 1;
+					}
+				}
+
+
+			}
+			else if (entity_y == y){
+
+				clearInterval(short_move);
+				
+				let move_x = setInterval(function(){
+					$("#display_coords").html(`X : ${entity_x} &nbsp;&nbsp;&nbsp;Y : ${entity_y}`);
+					if (entity_x != x){
+						if (entity_x > x){
+							entity_x -= 1;
+						}
+						else if (entity_x < x){
+							entity_x += 1;
+						}
+					}
+					else if (entity_x == x){
+						clearInterval(move_x);
+						moving = false;
+	
+
+					}
+					ENTITY.style.left = entity_x;
+					ENTITY.style.top = entity_y;
+
+				},5);
+			}
+			ENTITY.style.left = entity_x;
+			ENTITY.style.top = entity_y;
+
+
+		},5);
+
+	}
+	else {
+
+		let short_move = setInterval(function(){
+			$("#display_coords").html(`X : ${entity_x} &nbsp;&nbsp;&nbsp;Y : ${entity_y}`);
+			if (entity_x != x){
+				if (entity_x > x){
+					entity_x -= 1;
+					if (entity_y > y){
+						entity_y -= 1;
+					}
+					else if (entity_y < y){
+						entity_y += 1;
+					}
+				}
+				else if (entity_x < x){
+					entity_x += 1;
+					if (entity_y > y){
+						entity_y -= 1;
+					}
+					else if (entity_y < y){
+						entity_y += 1;
+					}
+				}
+
+
+			}
+			else if (entity_x == x){
+
+				clearInterval(short_move);
+				
+				let move_y = setInterval(function(){
+					$("#display_coords").html(`X : ${entity_x} &nbsp;&nbsp;&nbsp;Y : ${entity_y}`);
+
+					if (entity_y != y){
+						if (entity_y > y){
+							entity_y -= 1;
+						}
+						else if (entity_y < y){
+							entity_y += 1;
+						}
+					}
+					else if (entity_y == y){
+						clearInterval(move_y);
+						moving = false;
+
+
+					}
+					ENTITY.style.left = entity_x;
+					ENTITY.style.top = entity_y;
+
+				},5);
+			}
+			ENTITY.style.left = entity_x;
+			ENTITY.style.top = entity_y;
+
+
+		},5);
+
+
+	}
+
+
+
+
+}
+
 
 
 
